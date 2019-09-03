c695d145f7db98cef81fc386c7ae602768525817
herreradiego <m.diego.herrera@gmail.com>
2019-09-03 11:38:38 -0300

    Updates

---
+++ precache-manifest.b4dd7ecdfbb8f6be065791f1467b25fe.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bb9098351db81bb653c010380d96b564",
+    "url": "/digitalmente/index.html"
+  },
+  {
+    "revision": "9a324186cf5ad75c8f80",
+    "url": "/digitalmente/static/css/main.da1c7395.chunk.css"
+  },
+  {
+    "revision": "0926bdfb6ce391275e75",
+    "url": "/digitalmente/static/js/2.c10ba7c1.chunk.js"
+  },
+  {
+    "revision": "9a324186cf5ad75c8f80",
+    "url": "/digitalmente/static/js/main.a8207f1d.chunk.js"
+  },
+  {
+    "revision": "cacf0fa0dea3190aa7ca",
+    "url": "/digitalmente/static/js/runtime~main.fac9b579.js"
+  },
+  {
+    "revision": "363ccab4dc3cf912472e41b6a0aeeedd",
+    "url": "/digitalmente/static/media/app_img_01.363ccab4.png"
+  },
+  {
+    "revision": "ef82325d7c70cec99fa679ba9931d1b1",
+    "url": "/digitalmente/static/media/app_img_02.ef82325d.png"
+  },
+  {
+    "revision": "c059c1ee52119316250745440f0c6a39",
+    "url": "/digitalmente/static/media/app_img_03.c059c1ee.png"
+  },
+  {
+    "revision": "93d629126d6584de110cf081a03dfbfc",
+    "url": "/digitalmente/static/media/app_img_04.93d62912.png"
+  },
+  {
+    "revision": "e0c06dbb5d852dc19e70bad8c9cc02c4",
+    "url": "/digitalmente/static/media/headerImg.e0c06dbb.png"
+  },
+  {
+    "revision": "2cd8385e63a6799e364de8a90d3dd1a3",
+    "url": "/digitalmente/static/media/logo.2cd8385e.jpg"
+  },
+  {
+    "revision": "243f2c025f8fd615b5ed2452c533520f",
+    "url": "/digitalmente/static/media/selector_01.243f2c02.png"
+  },
+  {
+    "revision": "ce9953a0da597bf8a416b687287d8b7e",
+    "url": "/digitalmente/static/media/selector_02.ce9953a0.png"
+  },
+  {
+    "revision": "65eb0829a74275741924ba8adda08551",
+    "url": "/digitalmente/static/media/selector_03.65eb0829.png"
+  },
+  {
+    "revision": "8833214d6d036820a81c9fdd67cac777",
+    "url": "/digitalmente/static/media/selector_04.8833214d.png"
+  }
+]);
